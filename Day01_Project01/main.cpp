db0588ba7977fa21db388ea15bc78cd026e1c029
HyunOSung <sierrahotel555@gmail.com>
2019-06-11 18:20:18 +0900

    190611 수업 내용 커밋

--- Day01_Project01/main.cpp
+++ Day01_Project01/main.cpp
@@ -12,11 +12,10 @@
 
 //최적화의 시작은 다르다는 것에서 부터 시작, 컴파일러가 다르면 결과물도 다르다.
 
-#include<stdio.h>
-#include <stdlib.h>
-#include<iostream>
-#include<string.h>
-#include<conio.h>
+#include"engine.h"
+#include"Player.h"
+#include"Map.h"
+#include"Util.h"
 
 #define LEFT 75
 #define RIGHT 77
@@ -55,88 +54,13 @@
 //}
 
 
-int PlayerX = 1;
-int PlayerY = 1;
-
-int map[10][10] = {
-	{1,1,1,1,1,1,1,1,1,1},
-	{1,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,1},
-	{1,1,1,1,1,1,1,1,1,1}
-};
-
-int Input()
-{
-	int keyCode =  getch();
-
-	return keyCode;
-
-}
-
-void Tick(int keyCode)
-{
-	switch (keyCode)
-	{
-	case RIGHT:
-		PlayerX++;
-		if (PlayerX >= 10)
-		{
-			printf("d", map[][9]);
-		}
-		break;
-	case LEFT:
-		PlayerX--;
-		break;
-	case UP:
-		PlayerY--;
-		break;
-	case DOWN:
-		PlayerY++;
-		break;
-	}
-
-}
-
-void Draw()
-{
-	system("cls");
-	for (int y = 0; y < 10; ++y)
-	{
-		for (int x = 0; x < 10; ++x)
-		{
-			if (x == PlayerX && y == PlayerY)
-			{
-				printf("@ ");
-			}
-			else
-			{
-				printf("%d ", map[y][x]);
-			}
-
-			
-		}
-		printf("\n");
-	}
-}
-
-
-
-
-
 
 
 int main()
 {
-	while (1)
-	{
-		int keyCode = Input();
-		Tick(keyCode);
-		Draw();
-	}
+	Engine* engine = new Engine(); //필요한 함수만 불러오도록
+	engine->Run();  //(*engine).Run();
+	delete engine;  //설정한 영역에 들어갈 메모리 확보
+	return 0;
+
 }
